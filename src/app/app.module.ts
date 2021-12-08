aeb4fea884fb489f4de64eab33a9552ff3948b1f
Dulce Maria Martinez Garcia <dulcegarcia4545@gmail.com>
2021-12-07 19:33:54 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,19 +28,6 @@ import { SwPush} from '@angular/service-worker';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-  readonly VAPID_PUBLIC_KEY = "BLBx-hf2WrL2qEa0qKb-aCJbcxEvyn62GDTyyP9KTS5K7ZL0K7TfmOKSPqp8vQF0DaG8hpSBknz_x3qf5F4iEFo";
+export class AppModule {
 
-  constructor(
-      private swPush: SwPush,
-      private newsletterService: NewsletterService) {}
-
-  subscribeToNotifications() {
-
-      this.swPush.requestSubscription({
-          serverPublicKey: this.VAPID_PUBLIC_KEY
-      })
-      .then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())
-      .catch(err => console.error("Could not subscribe to notifications", err));
-  }
 }
